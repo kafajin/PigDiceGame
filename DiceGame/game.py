53a2255c7a81a92be23def0a867317a7af39fde9
fatimakadhem <fatimaraedkadhem@icloud.com>
2024-04-26 16:59:54 +0200

    update game.py class

--- DiceGame/game.py
+++ DiceGame/game.py
@@ -2,51 +2,91 @@ from computer_intelligence import ComputerIntelligence
 from highscore import HighScore
 
 
+high_scores = HighScore()
+
+
 class Game:
-    def __init__(self, players, dice):
+    """
+    Represents a game of Pig Dice.
+
+    Attributes:
+        players (list): A list of players participating in the game.
+        current_player_index (int): index of the current player in the players list.
+        dice: the dice used in the game.
+        high_scores: the high scores manager for the game.
+        turn (int): the current turn number in the game.
+    """
+
+    def __init__(self, players, dice, high_scores):
+        """
+        Initialize the Game object.
+
+        Args:
+            players (list): A list of Player objects representing the players in the game.
+            dice: The dice object used in the game.
+            high_scores: The high scores manager for the game.
+        """
         self.players = players
         self.current_player_index = 0
         self.dice = dice
-        self.high_scores = HighScore()
+        self.high_scores = high_scores
         self.turn = 0
 
     def add_player(self, player):
+        """
+        Add a player to the game.
+
+        Args:
+            player: The Player object to be added to the game.
+        """
         self.players.append(player)
 
     def switch_player(self):
-        self.current_player_index = (self.current_player_index + 1) % len(self.players)
+        """Switch to the next player in the game."""
+        self.current_player_index = (
+            self.current_player_index + 1) % len(self.players)
 
     def play(self):
+        """Start and manages the game."""
         print("\nWelcome to Pig Dice Game!\n")
         for player in self.players:
             print(f"Player {player.name} joined the game.")
 
-        while all(player.get_score() < 50 for player in self.players):
+        while all(player.get_score() < 100 for player in self.players):
             print("\n------------------------------")
 
             current_player = self.players[self.current_player_index]
 
             if isinstance(current_player, ComputerIntelligence):
                 print("Computer is playing...")
-                decision = current_player.take_turn(self.players[self.current_player_index - 1].get_score(), sum(player.get_score() for player in self.players))
+                scores = sum(player.get_score() for player in self.players)
+                decision = current_player.take_turn(scores)
                 if decision == "roll":
                     dice_roll = self.dice.roll_dice()
                     print(f"Computer rolled a {dice_roll}.")
                     current_player.add_score(dice_roll)
-                    print(f"Computer's current score: {current_player.get_score()}")
+                    current_score = current_player.get_score()
+                    print(f"Computer's current score: {current_score}")
                     if dice_roll == 1:
-                        print("Computer rolled a 1. No points gained this turn.")
+                        print("Computer rolled a 1.")
+                        print("No points gained this turn.")
+                        current_player.reset_score()
+                else:
+                    print("Computer chose to hold.")
+            else:
+                choice = current_player.take_turn(self.dice)
+                if choice == 'roll':
+                    dice_roll = self.dice.roll_dice()
+                    print(f"{current_player.name} rolled a {dice_roll}.")
+                    if dice_roll == 1:
+                        print("A 1 is rolled. No points gained this turn.")
                         current_player.reset_score()
                     else:
-                        print("Computer chose to hold.")
-            else:
-                choice = input("Do you want to roll or hold? (r/h) or type 'q' to quit and restart the game: ").lower()
-                if choice == 'q':
-                    print("Quitting the current game...")
-                    return True  # signal to restart the game
-                elif choice == 'r':
-                    current_player.take_turn(self.dice)
-                elif choice == 'h':
+                        print(f"{current_player.name} chose to roll again.")
+                        current_player.add_score(dice_roll)
+                        current_score = current_player.get_score()
+                        print(f"Your current score: {current_score}")
+                elif choice == 'hold':
                     print(f"{current_player.name} chose to hold.")
                     self.switch_player()
 
@@ -54,10 +94,8 @@ class Game:
             self.turn += 1
 
         winner = max(self.players, key=lambda player: player.get_score())
-        print(f"\nCongratulations, {winner.name} wins with {winner.get_score()} points!")
 
-        # Update high scores
         self.high_scores.update_high_scores(winner.name, winner.get_score())
+        print("Updated highscors:", self.high_scores.high_scores)
 
-        # Display the scores
-        self.high_scores.display_high_scores()
+        self.high_scores.save_high_scores()
