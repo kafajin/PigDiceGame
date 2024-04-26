cf1c5b9bddd8af15f3b5b5c1e7e61b2b0b203bff
fatimakadhem <fatimaraedkadhem@icloud.com>
2024-04-26 17:01:46 +0200

    updated the player class

--- DiceGame/player.py
+++ DiceGame/player.py
@@ -1,9 +1,10 @@
+import sys
+
+
 class Player:
-    # Simple class with 'name', 'score' and round_score as attributes.
     def __init__(self, name):
         self.name = name
         self.score = 0
-        # self.round_score = 0
 
     def reset_score(self):
         self.score = 0
@@ -13,29 +14,36 @@ class Player:
 
     def get_score(self):
         return self.score
-    
-    # below for human player
+
     def take_turn(self, dice):
         print(f"It's {self.name}'s turn.")
-        roll = input("Roll or Hold? (r/h): ").lower()
+
+        roll = input("Roll, hold, quit or cheat? (r/h/q/c): ").lower()
         if roll == 'r':
             dice_roll = dice.roll_dice()
             print(f"{self.name} rolled a {dice_roll}.")
             if dice_roll == 1:
-                print("You rolled a 1. No points gained this turn.")
+                print("A 1 is rolled. No points gained this turn.")
                 self.reset_score()
                 return False
-              
+
             else:
                 self.add_score(dice_roll)
                 print(f"Your current score: {self.get_score()}")
         elif roll == 'h':
             print(f"{self.name} chose to hold.")
             return False
+        elif roll == 'q':
+            print("Quiting the game. Thanks for playing")
+            sys.exit()
+        elif roll == 'c':
+            print("Cheat code activated...")
+            self.score = 100
+            
+            return False
         else:
-            print("Invalid input. Please enter 'r' to roll or 'h' to hold.")
+            print("Invalid input. Please enter 'r' to roll or 'h' to hold or 'q' to quit.")
 
-    # Method for selecting a name
     def select_name(self):
         human_name = input("\nPlease enter your name: ")
         if not human_name:
@@ -44,7 +52,6 @@ class Player:
             self.name = human_name
             print(f"Player's name set to {human_name}.")
 
-    # Method for changing name.
     def change_name(self, new_playername):
         if not new_playername:
             raise ValueError("Name cannot be empty. Please enter a name.")
@@ -53,7 +60,6 @@ class Player:
             self.name = new_playername
             print(f"Player's name changed from {old_name} to {new_playername}")
 
-    # Method for holding the score.
     def hold(self):
-        self.score += self.round_score
         self.round_score = 0
+        self.score += self.round_score
